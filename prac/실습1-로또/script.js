62a691684a66bd86b97fa2f8013e965023e53c51
ukey77 <158002512+ukey77@users.noreply.github.com>
2024-06-11 18:46:12 +0900

    js_prac

---
+++ prac/실습1-로또/script.js
@@ -0,0 +1,32 @@
+﻿/* 
+// 1번
+let lotto = []; // 배열 변수 생성
+let ulList = document.getElementById('lotto');
+
+for(let i=0;i<6;i++){
+    let liList = document.createElement('li'); // li생성
+    let num = Math.floor(Math.random()*45) + 1; // 1 ~ 45 정수 생성
+    liList.innerHTML = num;
+    ulList.appendChild(liList);
+} */
+
+
+    let lotto=[];
+    let ulList = document.getElementById('lotto');
+
+    for(let i=0; i<6; i++){
+        let liList = document.createElement('li');
+        let num = Math.floor(Math.random()*45)+1; // 1~45정수 생성
+
+        for(let j in lotto){
+            while(num==lotto[j]){
+                num = Math.floor(Math.random()*45)+1;
+            };
+        };
+
+        lotto.push(num);  //lotto배열에다 랜덤 num배열 넣기
+        liList.innerText = num;
+        liList.classList.add('num'+[num]);
+        ulList.appendChild(liList);
+
+    }
