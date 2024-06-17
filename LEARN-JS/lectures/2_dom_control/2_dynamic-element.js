a31612872d8ead662d635b1a2a698436fb456fad
ukey77 <158002512+ukey77@users.noreply.github.com>
2024-06-17 14:26:17 +0900

    #6

--- LEARN-JS/lectures/2_dom_control/2_dynamic-element.js
+++ LEARN-JS/lectures/2_dom_control/2_dynamic-element.js
@@ -8,13 +8,14 @@ let addBeforeBtn = document.getElementById('before'); //앞에추가 btn
 let target = document.getElementById('target'); // li요소
 
 // 추가 btn 누르면 현재 input값이 li로 추가되는 코드
-addBtn.addEventListener('click', function () {
+/* addBtn.addEventListener('click', function () {
   let cretLi = document.createElement('li');
   let iputTxt = document.querySelector('input');
   cretLi.textContent = iputTxt.value;
   listUl.appendChild(cretLi);
   iputTxt.value = '';
-});
+}); */
+
 // 앞에추가 btn 누르면 현재 input값이 스쿼드 상단으로 추가되는 코드
 addBeforeBtn.addEventListener('click', function () {
   let cretLi = document.createElement('li');
@@ -30,3 +31,36 @@ let removeTargetBtn = document.querySelector('#targetRm');
 removeTargetBtn.addEventListener('click', function () {
   target.remove();
 });
+
+// 추가 btn 클릭시 li와 btn 요소 함께 생성
+
+addBtn.addEventListener('click', function () {
+  // btn 생성
+  let creatBtn = document.createElement('button'); // 버튼요소 생성
+  // creatBtn.classList.add('removeBtn'); // 클래스 추가 방법 1
+  creatBtn.setAttribute('class', 'removeBtn'); //  // 클래스 추가 방법 2
+  creatBtn.type = 'button'; // 버튼요소의 type 설정
+  creatBtn.textContent = 'X'; // 버튼요소의 text 설정
+  creatBtn.addEventListener('click', removeParentNode);
+
+  // li 생성
+  let cretLi = document.createElement('li'); // li요소 생성
+  let iputTxt = document.querySelector('input'); // input요소 가져오기
+  cretLi.textContent = iputTxt.value; // li의 text에 input요소 value 값 대입
+  cretLi.appendChild(creatBtn); // li에 버튼요소 삽입
+  listUl.appendChild(cretLi); // ul에 li 삽입
+
+  iputTxt.value = ''; // 추가된 후 input 값 초기화
+  iputTxt.focus(); // input박스에 focus놓기
+});
+
+//X버튼 클릭시 해당 부모인 li 요소가 삭제
+let removeBtn = document.querySelector('.removeBtn'); // li요소의 X버튼
+
+removeBtn.addEventListener('click', removeParentNode);
+
+function removeParentNode(event) {
+  event.target.parentNode.remove();
+  let iputTxt = document.querySelector('input');
+  iputTxt.focus();
+}
